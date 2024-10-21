9e7c6b938571a0176b08bd313f8076f3ca42f623
Matt Ashby <matt@ashbys.nz>
2024-10-21 17:07:33 +1300

    Refactor PowerShell script to install Microsoft.Graph module if not already installed and retrieve enterprise applications with Windows Azure Active Directory integration tags, exporting details to CSV

--- enterpriseapps.ps1
+++ enterpriseapps.ps1
@@ -1,10 +1,13 @@
-Import-Module Microsoft.Graph.Applications
+# Install the Microsoft.Graph module if not already installed
+if (-not (Get-Module -ListAvailable -Name Microsoft.Graph)) {
+    Install-Module Microsoft.Graph -Scope CurrentUser -Force
+}
 
 # Connect to Microsoft Graph
 Connect-MgGraph -Scopes "Application.Read.All"
 
 # Get all enterprise applications
-$enterpriseApps = Get-MgServicePrincipal -All
+$enterpriseApps = Get-MgServicePrincipal -Filter "tags/any(t:t eq 'WindowsAzureActiveDirectoryIntegratedApp')"
 
 # Display the enterprise applications
 $enterpriseApps | Select-Object DisplayName, Description, Homepage | Export-csv -Path "C:\temp\enterpriseapps.csv" -NoTypeInformation
